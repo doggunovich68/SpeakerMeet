5c07332e6886f24dcbe774032d504a8f260163c2
Sergey Doggunovich <sharcomovie68@gmail.com>
2021-01-15 01:21:53 +0300

    files was added

---
+++ SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SpeakerMeet.API.Tests
+{
+    public class SpeakerControllerSearchTests
+    {
+        [Fact]
+        public void ItExists()
+        {
+	var controller = new SpeakerController();
+        }
+    }
+}
