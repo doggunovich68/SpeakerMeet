220413fa8782e81b33afc614d72ee2f8e99770cd
Sergey Doggunovich <sharcomovie68@gmail.com>
2021-01-21 19:08:46 +0300

    10 test passed

--- SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
+++ SpeakerMeet.API.Tests/SpeakerControllerSearchTests.cs
@@ -3,6 +3,7 @@ using Xunit;
 using SpeakerMeet.API.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SpeakerMeet.API.Tests
 {
@@ -37,5 +38,45 @@ public class SpeakerControllerSearchTests
             Assert.NotNull(result.Value);
             Assert.IsType<List<Speaker>>(result.Value);
         }
+        [Fact]
+        public void GivenExactMatchThenOneSpeakerInCollection()
+        {
+            var result = _controller.Search("Joshua") as OkObjectResult;
+            var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
+            Assert.Equal(1, speakers.Count);
+        }
+	    [Theory]
+	    [InlineData("Joshua")]
+	    [InlineData("joshua")]
+	    [InlineData("JoShUa")]
+	    public void GivenCaseInsensitveMatchThenSpeakerInCollection (string searchString)
+	    {
+		    var result = _controller.Search(searchString) as OkObjectResult;
+		    var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
+		    Assert.Equal(1, speakers.Count);
+		    Assert.Equal("Joshua", speakers[0].Name);
+        } 
+        [Fact]
+        public void GivenNoMatchThenEmptyCollection()
+        {
+            var result = _controller.Search("ZZZ") as OkObjectResult;
+            var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
+            Assert.Equal(0, speakers.Count);
+        }
+        [Fact]
+        public void Given3MatchThenCollectionWith3Speakers()
+        {
+            var result = _controller.Search("jos") as OkObjectResult;
+            var speakers = ((IEnumerable<Speaker>)result.Value).ToList();
+            Assert.Equal(3, speakers.Count);
+            Assert.True(speakers.Any(s => s.Name == "Josh"));
+            Assert.True(speakers.Any(s => s.Name == "Joshua"));
+            Assert.True(speakers.Any(s => s.Name == "Joseph"));
+        }
+        private readonly SpeakerController _controller;
+        public SpeakerControllerSearchTests()
+            {
+            _controller = new SpeakerController();
+            }
     }
 }
